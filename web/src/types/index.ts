44629cdec1dfe285335ee49b8137ab5aec35ce25
Ben Pinner <benpinner1997@gmail.com>
2022-02-03 14:06:37 +0000

    fixed spelling mistake

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -1,4 +1,4 @@
-// My preferred solution to copying to interface from the api
+// My preferred solution to copying the interface from the api
 // is to set up a third project that stores all shared types
 // and have that project as a dependency of the api and web
 // projects.
