9ed9a0d62c9cd6ade89cbb5a4918c3276fde615d
Ben Pinner <benpinner1997@gmail.com>
2022-02-10 10:53:07 +0000

    fixed get request address in web project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC<Props> = () => {
     const fetchTimes = async () => {
       const response = await axios({
         method: `GET`,
-        url: `http://127.0.0.1:3000/bus-times`,
+        url: `http://localhost:3000/bus-times`,
       });
 
       const newTimes: BusTime[] = response.data;
